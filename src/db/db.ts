25228b435bd0c1f1f4b2296a43f214480820137a
DantesChmo <doni-rio@yandex.ru>
2021-07-01 15:20:51 +0300

    adding host

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,11 +2,10 @@ import { PgClient } from '../lib/PgClient';
 import { RedisClient } from '../lib/RedisClient';
 
 const pgClient = new PgClient({
-  host: 'database',
+  host: process.env.POSTGRES_HOST || 'database',
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  string: process.env.POSTGRES_CONNECTION_STRGING || 'postgres://db_user:db_user@database:5432/words'
 });
 
 const redisClient = new RedisClient({});
